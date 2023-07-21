ec29fa7b430d5fb8376c5d28a75cfc370eb1caf3
Gabriel P??rez <gabriel@garox.org>
2023-07-21 19:40:54 -0400

    Updates

---
+++ _next/static/gAtAUNVyCz9lTkpRwE1J7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/310-09457970002a4229.js","static/chunks/pages/index-e38a27902bddef8b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
